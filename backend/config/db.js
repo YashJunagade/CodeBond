a250456d01ed45126b5108de580b5fa4caa9191c
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-05-14 18:13:47 +0530

    seting up with the mongodb db

---
+++ backend/config/db.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+
+    // Handle MongoDB connection errors after initial connection
+    mongoose.connection.on("error", (err) => {
+      console.error(`MongoDB connection error: ${err}`);
+    });
+
+    mongoose.connection.on("disconnected", () => {
+      console.warn("MongoDB disconnected");
+    });
+
+    // Handle Node process termination and close MongoDB connection properly
+    process.on("SIGINT", async () => {
+      await mongoose.connection.close();
+      console.log("MongoDB connection closed due to app termination");
+      process.exit(0);
+    });
+  } catch (error) {
+    console.error(`MongoDB connection error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
