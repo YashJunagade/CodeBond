5b871b6bc9c4ae22a27b9a0d0c2ea8e718cd1014
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-05-14 18:12:00 +0530

    setup for toast popup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,22 @@ import Navbar from './components/navbar/Header'
 import Problems from './pages/Problems'
 import CodeEditor from './pages/CodeEditor'
 import CustomProblem from './pages/CustomProblem'
+import { Toaster } from 'react-hot-toast'
 
 const App = () => {
   return (
-    <Router>
-      <Navbar user={{ name: 'You', avatar: 'https://i.pravatar.cc/100' }} />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/problems" element={<Problems />} />
-        <Route path="/codeEditor" element={<CodeEditor />} />
-        <Route path="/customProblem" element={<CustomProblem />} />
-      </Routes>
-    </Router>
+    <>
+      <Toaster position="bottom-right" />
+      <Router>
+        <Navbar user={{ name: 'You', avatar: 'https://i.pravatar.cc/100' }} />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/problems" element={<Problems />} />
+          <Route path="/codeEditor" element={<CodeEditor />} />
+          <Route path="/customProblem" element={<CustomProblem />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
