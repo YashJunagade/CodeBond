0a202f26b6301ad2cf7b686cf4fba16814710508
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-05-14 12:17:48 +0530

    added the real quotes updated every time when the component is reloaded

--- frontend/src/components/dashboard/MotivationQuote.jsx
+++ frontend/src/components/dashboard/MotivationQuote.jsx
@@ -1,4 +1,28 @@
-const MotivationQuote = ({ quote }) => {
+import React, { useState, useEffect } from 'react'
+
+const MotivationQuote = () => {
+  const [quote, setQuote] = useState({ text: '', author: '' })
+  const fetchQuote = async () => {
+    try {
+      const res = await fetch(
+        'https://api.allorigins.win/get?url=' +
+          encodeURIComponent('https://zenquotes.io/api/random') //will remove this cross origin after the deployment
+      )
+      const dataWrapped = await res.json()
+      const data = JSON.parse(dataWrapped.contents)
+      setQuote({
+        text: data[0].q,
+        author: data[0].a,
+      })
+    } catch (err) {
+      console.error('Failed to fetch quote', err)
+    }
+  }
+
+  useEffect(() => {
+    fetchQuote()
+  }, [])
+
   return (
     <div className="p-6 text-center mt-6 border-t dark:border-gray-600">
       <h3 className="text-md text-gray-600 dark:text-gray-400 italic mb-2">
