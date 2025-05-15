8a8c48428e2039a5956ad5e3077e6501ea2a60d2
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-05-15 20:45:55 +0530

    some more routes login, register and private route changed all routes to private except login and register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,20 +5,59 @@ import Problems from './pages/Problems'
 import CodeEditor from './pages/CodeEditor'
 import CustomProblem from './pages/CustomProblem'
 import { Toaster } from 'react-hot-toast'
+import Register from './pages/Register'
+import Login from './pages/Login'
+import { AuthProvider } from './context/AuthContext'
+import PrivateRoute from './PrivateRoute'
 
 const App = () => {
   return (
     <>
       <Toaster position="bottom-right" />
-      <Router>
-        <Navbar user={{ name: 'You', avatar: 'https://i.pravatar.cc/100' }} />
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/problems" element={<Problems />} />
-          <Route path="/codeEditor/:title" element={<CodeEditor />} />
-          <Route path="/customProblem" element={<CustomProblem />} />
-        </Routes>
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Navbar user={{ name: 'You', avatar: 'https://i.pravatar.cc/100' }} />
+          <Routes>
+            {/* Public Routes */}
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+
+            {/* Protected Routes */}
+            <Route
+              path="/"
+              element={
+                <PrivateRoute>
+                  <Dashboard />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/problems"
+              element={
+                <PrivateRoute>
+                  <Problems />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/codeEditor/:title"
+              element={
+                <PrivateRoute>
+                  <CodeEditor />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/customProblem"
+              element={
+                <PrivateRoute>
+                  <CustomProblem />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </Router>
+      </AuthProvider>
     </>
   )
 }
