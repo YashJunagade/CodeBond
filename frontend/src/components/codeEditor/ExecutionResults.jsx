2fca2655589630657bdc81bf9d45d92b494b4018
Yash Anil Junagade <yash.junagade1900@gmail.com>
2025-05-16 03:27:57 +0530

    services for the submission

--- frontend/src/components/codeEditor/ExecutionResults.jsx
+++ frontend/src/components/codeEditor/ExecutionResults.jsx
@@ -1,3 +1,4 @@
+// src/components/CodeEditor/ExecutionResults.js
 import React, { useState } from 'react'
 import {
   CheckCircle,
---
+++ frontend/src/services/submissionService.js
@@ -0,0 +1,9 @@
+import axios from 'axios'
+
+const API_BASE_URL = 'http://localhost:5000/api'
+
+export const createSubmission = async (submissionData) => {
+  const res = await axios.post(`${API_BASE_URL}/submissions`, submissionData)
+  console.log(`${API_BASE_URL}/submissions`, submissionData)
+  return res.data
+}
